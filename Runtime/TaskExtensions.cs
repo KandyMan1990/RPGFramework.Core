526a05200d94ad360193b69fc0831792e9ba0d4f
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2025-11-03 21:35:27 +0000

    Added FireAndForget Task extension

---
+++ Runtime/TaskExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RPGFramework.Core
+{
+    public static class TaskExtensions
+    {
+        public static void FireAndForget(this Task task, Action<Exception> onException = null, bool continueOnMainThread = false)
+        {
+            _ = HandleTaskAsync(task, onException, continueOnMainThread);
+        }
+
+        private static async Task HandleTaskAsync(Task task, Action<Exception> onException, bool continueOnMainThread)
+        {
+            try
+            {
+                await task.ConfigureAwait(!continueOnMainThread);
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    onException?.Invoke(ex);
+                }
+                catch (Exception handlerEx)
+                {
+                    Debug.LogError($"FireAndForget exception handler threw: {handlerEx}");
+                }
+
+                if (onException == null)
+                {
+                    Debug.LogException(ex);
+                }
+            }
+        }
+    }
+}
