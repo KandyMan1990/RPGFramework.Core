ec3588642e5b30645191d02bc9e48200d7f13108
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2025-11-20 22:27:37 +0000

    Fix issues with RPGUIButton not showing/triggering events correctly

--- Runtime/UI/RPGUIButton.cs
+++ Runtime/UI/RPGUIButton.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using UnityEngine.UIElements;
+﻿using UnityEngine.UIElements;
 
 namespace RPGFramework.Core.UI
 {
     [UxmlElement]
     public partial class RPGUIButton : VisualElement
     {
-        public static event Action OnClicked;
-        public static event Action OnHighlighted;
-
         [UxmlAttribute]
         public string text
         {
@@ -19,7 +15,7 @@ namespace RPGFramework.Core.UI
         private readonly Image m_Icon;
         private readonly Label m_Label;
 
-        private bool m_HoverOrFocus;
+        private bool m_IsHoveringOrFocused;
 
         public RPGUIButton()
         {
@@ -27,118 +23,61 @@ namespace RPGFramework.Core.UI
 
             m_Icon = new Image();
             m_Icon.AddToClassList("rpg-ui-button-icon");
-            ShowIcon(false);
+            m_Icon.style.opacity = 0f;
+            m_Icon.focusable     = false;
 
             m_Label      = new Label();
             m_Label.text = "New Text";
             m_Label.AddToClassList("rpg-ui-label");
+            m_Label.focusable = false;
 
             Add(m_Icon);
             Add(m_Label);
 
-            RegisterCallback<AttachToPanelEvent>(evt =>
-                                                 {
-                                                     focusable   = true;
-                                                     pickingMode = PickingMode.Position;
-                                                 });
+            pickingMode = PickingMode.Position;
 
-            RegisterCallback<AttachToPanelEvent>(OnAttach);
-            RegisterCallback<DetachFromPanelEvent>(OnDetach);
-
-            RegisterCallback<PointerEnterEvent>(OnPointerEnter);
-            RegisterCallback<PointerLeaveEvent>(OnPointerLeave);
+            RegisterCallback<AttachToPanelEvent>(OnAttachToPanel);
             RegisterCallback<FocusInEvent>(OnFocusIn);
-            RegisterCallback<BlurEvent>(OnBlur);
-
-            RegisterCallback<ClickEvent>(OnClick);
-            RegisterCallback<NavigationSubmitEvent>(OnSubmit);
+            RegisterCallback<FocusOutEvent>(OnFocusOut);
+            RegisterCallback<PointerEnterEvent>(OnPointerEnter);
         }
 
-        private void OnAttach(AttachToPanelEvent evt)
+        private void OnAttachToPanel(AttachToPanelEvent evt)
         {
-            VisualElement root = panel?.visualTree;
-
-            root?.RegisterCallback<FocusInEvent>(OnAnyFocus, TrickleDown.TrickleDown);
-            root?.RegisterCallback<PointerEnterEvent>(OnAnyHover, TrickleDown.TrickleDown);
-        }
-
-        private void OnDetach(DetachFromPanelEvent evt)
-        {
-            VisualElement root = panel?.visualTree;
-
-            root?.UnregisterCallback<FocusInEvent>(OnAnyFocus, TrickleDown.TrickleDown);
-            root?.UnregisterCallback<PointerEnterEvent>(OnAnyHover, TrickleDown.TrickleDown);
-        }
-
-        private void OnAnyFocus(FocusInEvent evt)
-        {
-            bool showIcon = evt.target == this;
-
-            ShowIcon(showIcon);
-        }
-
-        private void OnAnyHover(PointerEnterEvent evt)
-        {
-            bool showIcon = evt.target == m_Label;
-
-            ShowIcon(showIcon);
-        }
-
-        private void OnPointerEnter(PointerEnterEvent evt)
-        {
-            RaiseHighlightedEvent();
-        }
-
-        private void OnPointerLeave(PointerLeaveEvent evt)
-        {
-            ResetHighlightedEvent();
+            focusable = true;
+            UnregisterCallback<AttachToPanelEvent>(OnAttachToPanel);
         }
 
         private void OnFocusIn(FocusInEvent evt)
         {
-            RaiseHighlightedEvent();
+            if (evt.target == this)
+            {
+                ShowHighlight(true);
+            }
         }
 
-        private void OnBlur(BlurEvent evt)
+        private void OnFocusOut(FocusOutEvent evt)
         {
-            ResetHighlightedEvent();
+            if (evt.target == this)
+            {
+                ShowHighlight(false);
+            }
         }
 
-        private void RaiseHighlightedEvent()
+        private void OnPointerEnter(PointerEnterEvent evt)
         {
-            if (m_HoverOrFocus)
+            Focus();
+        }
+
+        private void ShowHighlight(bool show)
+        {
+            if (show == m_IsHoveringOrFocused)
             {
                 return;
             }
-            m_HoverOrFocus = true;
 
-            OnHighlighted?.Invoke();
-        }
-
-        private void ResetHighlightedEvent()
-        {
-            m_HoverOrFocus = false;
-        }
-
-        private void OnSubmit(NavigationSubmitEvent evt)
-        {
-            if (focusController.focusedElement == this)
-                RaiseHighlightedEvent();
-
-            OnClicked?.Invoke();
-        }
-
-        private void OnClick(ClickEvent evt)
-        {
-            if (focusController.focusedElement == this)
-                RaiseHighlightedEvent();
-
-            OnClicked?.Invoke();
-        }
-
-        private void ShowIcon(bool show)
-        {
-            m_Icon.style.opacity = show ? 1f : 0f;
+            m_IsHoveringOrFocused = show;
+            m_Icon.style.opacity  = show ? 1f : 0f;
         }
     }
 }
