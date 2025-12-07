b69b2d0f09eb4c030203405af13246a5789fd639
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2025-12-07 18:27:26 +0000

    Add SetEnabledAndVisible UI extension method

---
+++ Runtime/UI/UIExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.UIElements;
+
+namespace RPGFramework.Core.UI
+{
+    public static class UIExtensions
+    {
+        public static void SetEnabledAndVisible(this VisualElement visualElement, bool enabled)
+        {
+            visualElement.SetEnabled(enabled);
+            visualElement.style.display = enabled ? DisplayStyle.Flex : DisplayStyle.None;
+        }
+    }
+}
