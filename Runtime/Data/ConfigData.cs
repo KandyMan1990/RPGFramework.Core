b4e861c12dd7f99ba74335ed664af64354de3855
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2026-01-09 22:01:50 +0000

    Create GlobalConfig service and example config data

---
+++ Runtime/Data/ConfigData.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace RPGFramework.Core.Data
+{
+    /// <summary>
+    /// Example config data using the settings in Menu.ConfigMenu.  If a custom ConfigMenu is created with more options, just clone this struct with the additional properties added and use it instead
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct ConfigData
+    {
+        public fixed byte  Language[8];
+        public       float MusicVolume;
+        public       float SfxVolume;
+        public       float BattleMessageSpeed;
+        public       float FieldMessageSpeed;
+        // TODO: control bindings
+
+        public string GetLanguage()
+        {
+            fixed (byte* ptr = Language)
+            {
+                int length = 0;
+                while (length < 8 && ptr[length] != 0)
+                {
+                    length++;
+                }
+
+                return Encoding.ASCII.GetString(ptr, length);
+            }
+        }
+
+        public void SetLanguage(string value)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Language[i] = i < value.Length ? (byte)value[i] : (byte)0;
+            }
+        }
+    }
+}
---
+++ Runtime/GlobalConfig/GlobalConfig.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace RPGFramework.Core.GlobalConfig
+{
+    public interface IGlobalConfig
+    {
+        /// <summary>
+        /// Loads a config file from the system using the provided struct for the format
+        /// </summary>
+        /// <typeparam name="T">Must be a blittable type</typeparam>
+        /// <returns>A config file of type T</returns>
+        bool TryGet<T>(out T value) where T : unmanaged;
+        /// <summary>
+        /// Saves a config file to the system
+        /// </summary>
+        /// <param name="value">The data to be saved to disk</param>
+        /// <typeparam name="T">Must be a blittable type</typeparam>
+        /// <returns></returns>
+        void Set<T>(T value) where T : unmanaged;
+    }
+
+    public class GlobalConfig : IGlobalConfig
+    {
+        private const string FILENAME = "global.config";
+
+        private readonly string m_Path = Path.Combine(Application.persistentDataPath, FILENAME);
+
+        private byte[] m_Data;
+
+        bool IGlobalConfig.TryGet<T>(out T value)
+        {
+            if (m_Data == null)
+            {
+                if (!File.Exists(m_Path))
+                {
+                    value = default;
+                    return false;
+                }
+
+                m_Data = File.ReadAllBytes(m_Path);
+            }
+
+            value = MemoryMarshal.Read<T>(m_Data);
+            return true;
+        }
+
+        unsafe void IGlobalConfig.Set<T>(T value)
+        {
+            m_Data = new byte[sizeof(T)];
+            MemoryMarshal.Write(m_Data, ref value);
+
+            File.WriteAllBytes(m_Path, m_Data);
+        }
+    }
+}
