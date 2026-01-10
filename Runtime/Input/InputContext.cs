f6ce60cdfe2d9a61be2e8acb4df1f3973a15ddb2
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2026-01-10 20:12:13 +0000

    Added IInputContext

---
+++ Runtime/Input/InputContext.cs
@@ -0,0 +1,21 @@
+﻿namespace RPGFramework.Core.Input
+{
+    public enum ControlSlot
+    {
+        Primary,
+        Secondary,
+        Tertiary,
+        Quaternary,
+        ShoulderLeft,
+        ShoulderRight,
+        TriggerLeft,
+        TriggerRight,
+        Start,
+        Select
+    }
+
+    public interface IInputContext
+    {
+        bool Handle(ControlSlot slot);
+    }
+}
