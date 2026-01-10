cd593702dd84bf5e5860d4207ed09c1fa2cd2df3
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2026-01-10 20:12:29 +0000

    Added IInputRouter

---
+++ Runtime/Input/InputRouter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace RPGFramework.Core.Input
+{
+    public interface IInputRouter
+    {
+        void Push(IInputContext context);
+        void Pop(IInputContext  context);
+        void Clear();
+        void Route(ControlSlot slot);
+    }
+
+    public sealed class InputRouter : IInputRouter
+    {
+        private readonly Stack<IInputContext> m_Stack;
+
+        public InputRouter()
+        {
+            m_Stack = new Stack<IInputContext>();
+        }
+
+        void IInputRouter.Push(IInputContext context)
+        {
+            m_Stack.Push(context);
+        }
+
+        void IInputRouter.Pop(IInputContext context)
+        {
+            if (m_Stack.Count > 0 && m_Stack.Peek() == context)
+            {
+                m_Stack.Pop();
+            }
+        }
+
+        void IInputRouter.Clear()
+        {
+            m_Stack.Clear();
+        }
+
+        void IInputRouter.Route(ControlSlot slot)
+        {
+            foreach (IInputContext context in m_Stack)
+            {
+                if (context.Handle(slot))
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
