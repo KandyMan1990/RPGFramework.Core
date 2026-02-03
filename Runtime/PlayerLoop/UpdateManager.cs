20e8ee869579e5a70edad1007f8ae446cbdcb4d9
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2026-02-03 23:22:50 +0000

    Add deferred unregister method to UpdateManager

--- Runtime/PlayerLoop/UpdateManager.cs
+++ Runtime/PlayerLoop/UpdateManager.cs
@@ -4,16 +4,23 @@ namespace RPGFramework.Core.PlayerLoop
 {
     public static class UpdateManager
     {
-        private static readonly List<IUpdatable> m_Updatables = new List<IUpdatable>();
+        private static readonly List<IUpdatable>  m_Updatables = new List<IUpdatable>();
+        private static readonly Queue<IUpdatable> m_Queue      = new Queue<IUpdatable>();
 
-        public static void RegisterUpdatable(IUpdatable   player) => m_Updatables.Add(player);
-        public static void UnregisterUpdatable(IUpdatable player) => m_Updatables.Remove(player);
+        public static void RegisterUpdatable(IUpdatable           player) => m_Updatables.Add(player);
+        public static void UnregisterUpdatable(IUpdatable         player) => m_Updatables.Remove(player);
+        public static void QueueForUnregisterUpdatable(IUpdatable player) => m_Queue.Enqueue(player);
 
         public static void UpdateListeners()
         {
             foreach (IUpdatable player in m_Updatables)
             {
                 player.Update();
+            }
+
+            while (m_Queue.Count > 0)
+            {
+                UnregisterUpdatable(m_Queue.Dequeue());
             }
         }
 
